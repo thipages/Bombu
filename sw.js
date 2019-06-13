fa094733b22b0d9d8507cbb3a239e12b0acb7784
thipages <thipages@gmail.com>
2019-06-13 14:16:24 +0200

    pwa0.1

--- sw.js
+++ sw.js
@@ -1,24 +1,35 @@
-var cacheName = 'bombu-pwa';
-var filesToCache = [
-    './',
+const cacheName = 'bombu-v0.1';
+const assets = [
+    './', // stored in case of user
     './index.html',
     './style.css',
-    './index.js'
+    './index.js',
+    './king.jpg'
 ];
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-    e.waitUntil(
+self.addEventListener('install', event=>
+    event.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
+            return cache.addAll(assets);
         })
-    );
-});
+    )
+);
+/* Activate the service worker (after install event and all install (eg browser tabs) are closed) */
+self.addEventListener('activate', event=> {
+        // remove old cache names
+        event.waitUntil(
+            caches.keys().then(keys=> Promise.all(keys=>
+                keys
+                    .filter(key=>key!==cacheName)
+                    .map(key=> caches.delete(key))
+            )
+        ))
+    }
+);
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
-});
+self.addEventListener('fetch', event=>
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    )
+);
