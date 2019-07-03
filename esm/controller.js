e1cec2952283cce6464acdf94eb28ef1c8df497f
thipages <thipages@gmail.com>
2019-07-04 00:11:54 +0200

    remove lighterhtml dependencies from controller

--- esm/controller.js
+++ esm/controller.js
@@ -1,21 +1,12 @@
-const {render,html} = lighterhtml;
-const pages=(pageIndex,htmlList,cssClass, style)=> htmlList.map((h,index)=>
-    html`<div class="${cssClass}" style=${[pageIndex===index?'display:block':'display:none',style].join(";")}>${h}</div>`);
 export const EVENT=(type,data)=>({type,data});
 const pageListener=(listener,pageNum)=>(args)=> listener(pageNum,args);
-const functionList=(listener,registrar,sharedData)=>registrar.map((item,index)=>item(pageListener(listener,index),sharedData));
+const functionList=(listener,registrar,sharedData)=>registrar.map((r,index)=>r(pageListener(listener,index),sharedData));
 const htmlList=(fList)=>fList.map(f=>f());
-const update=(htmlList,functionList,selector, cssClass, style)=>(pageNum,args)=> {
-    if (pageNum===undefined)
-        pageNum=0;
-    else
-        htmlList[pageNum]=functionList[pageNum](args);
-    render(
-        selector,
-        ()=>html`${pages(pageNum,htmlList,cssClass, style)}`
-    );
-};
-export const register=(sharedData,listener,registrar,selector, cssClass, style)=>{
+const update=(htmlList,functionList,render)=>(pageNum,args)=>
+    (pageNum===undefined)
+        ? render(functionList[0](args))
+        : render(functionList[pageNum](args));
+export const register=(sharedData,listener,registrar, render)=>{
     let _functionList=functionList(listener,registrar,sharedData);
-    return update(htmlList(_functionList),_functionList,selector, cssClass, style);
+    return update(htmlList(_functionList),_functionList, render);
 };
--- esm/index.js
+++ esm/index.js
@@ -2,6 +2,7 @@ import {E_DONE, E_NOT_DONE, E_RESET, register as r0} from "./pages/page0";
 import {E1_ANNULER, E1_SCORE, E1_VALIDER, register as r1} from "./pages/page1";
 import {register} from "./controller";
 import {players, states} from "./model";
+const {render,html} = lighterhtml;
 const listener=(pageNum,args)=> {
     const same=()=>updater(pageNum,args);
     const page=(page)=>updater(page,args);
@@ -29,10 +30,13 @@ const listener=(pageNum,args)=> {
     }
 };
 const state=states(players.length,false);
+const renderer = (html) => render (
+    document.getElementById("app"),()=>html
+);
 const updater=register(
     {state},
     listener,
     [r0,r1],
-    document.getElementById("app")
+    renderer
 );
 updater();
