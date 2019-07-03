542a755dcaf07dad82e9f4bb4b7e735d8fbbace2
thipages <thipages@gmail.com>
2019-07-03 10:29:57 +0200

    controller refactoring

--- esm/controller.js
+++ esm/controller.js
@@ -2,32 +2,20 @@ const {render,html} = lighterhtml;
 const pages=(pageIndex,htmlList,cssClass, style)=> htmlList.map((h,index)=>
     html`<div class="${cssClass}" style=${[pageIndex===index?'display:block':'display:none',style].join(";")}>${h}</div>`);
 export const EVENT=(type,data)=>({type,data});
-const listener=(pageNum)=>(args)=> _listener(pageNum,args);
-const functionList=(registrar,sharedData)=>registrar.map((item,index)=>item(listener(index),sharedData));
+const pageListener=(listener,pageNum)=>(args)=> listener(pageNum,args);
+const functionList=(listener,registrar,sharedData)=>registrar.map((item,index)=>item(pageListener(listener,index),sharedData));
 const htmlList=(fList)=>fList.map(f=>f());
-const update=(pageNum,args)=> {
+const update=(htmlList,functionList,selector, cssClass, style)=>(pageNum,args)=> {
     if (pageNum===undefined)
         pageNum=0;
     else
-        _htmlList[pageNum]=_functionList[pageNum](args);
+        htmlList[pageNum]=functionList[pageNum](args);
     render(
-        _selector,
-        ()=>html`${pages(pageNum,_htmlList,_cssClass, _style)}`
+        selector,
+        ()=>html`${pages(pageNum,htmlList,cssClass, style)}`
     );
 };
 export const register=(sharedData,listener,registrar,selector, cssClass, style)=>{
-    _cssClass=cssClass;
-    _style=style;
-    _listener=listener;
-    _selector=selector;
-    _functionList=functionList(registrar,sharedData);
-    _htmlList=htmlList(_functionList);
-    return update;
+    let _functionList=functionList(listener,registrar,sharedData);
+    return update(htmlList(_functionList),_functionList,selector, cssClass, style);
 };
-let _functionList,
-    _htmlList,
-    _cssClass,
-    _style,
-    _selector,
-    _listener;
-
