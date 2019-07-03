6bf27b692e470be4201ef7434622979fc6d5d00b
thipages <thipages@gmail.com>
2019-07-04 01:39:14 +0200

    cleanup

--- esm/controller.js
+++ esm/controller.js
@@ -1,9 +1,5 @@
-export const EVENT=(type,data)=>({type,data});
 const pageListener=(listener,pageIndex)=>(event)=> listener(pageIndex,event);
 const functionList=(listener,registrar,sharedData)=>registrar.map((r,index)=>r(pageListener(listener,index),sharedData));
-const htmlList=(fList)=>fList.map(f=>f());
-const update=(htmlList,functionList,render)=>(pageIndex,event)=>render(functionList[pageIndex](event));
-export const register=(sharedData,listener,registrar, render)=>{
-    let _functionList=functionList(listener,registrar,sharedData);
-    return update(htmlList(_functionList),_functionList, render);
-};
+const update=(functionList,render)=>(pageIndex,event)=>render(functionList[pageIndex](event));
+export const EVENT=(type,data)=>({type,data});
+export const register=(sharedData,listener,registrar, render)=>update(functionList(listener,registrar,sharedData),render);
